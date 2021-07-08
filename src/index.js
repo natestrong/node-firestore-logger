630efa7cc299ca5733d71e1cdc3ee3654ef4cca4
nstrong <nstrong@spanimation.com>
2021-07-07 18:44:35 -0700

    change stuff

--- src/index.js
+++ src/index.js
@@ -2,9 +2,22 @@
 exports.__esModule = true;
 var firestore_1 = require("rxfire/firestore");
 var db_1 = require("./db");
-var usersCollection = db_1.firestore.collection('users');
+var yargs_1 = require("yargs");
+var argv = yargs_1["default"](process.argv).argv;
+var path = argv['path'];
+var usersCollection = db_1.firestore.collection(path);
+var initialEmission = true;
 firestore_1.collectionChanges(usersCollection)
     .subscribe(function (docChanges) {
+    if (initialEmission) {
+        console.log(docChanges.length + " docs in collection");
+        initialEmission = false;
+        return;
+    }
+    if (docChanges.length > 1) {
+        console.log(docChanges.length + " docs have been " + docChanges[0].type);
+        return;
+    }
     docChanges.forEach(function (docChange) {
         console.log("Document " + docChange.doc.id + " has been " + docChange.type);
     });
--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,28 @@ import {collectionChanges} from 'rxfire/firestore';
 import {firestore} from "./db";
 import firebase from "firebase";
 import DocumentChange = firebase.firestore.DocumentChange;
+import yargs from "yargs";
 
+const {argv} = yargs(process.argv);
+const path = argv['path'];
 
-const usersCollection = firestore.collection('users');
+const usersCollection = firestore.collection(path);
 
+let initialEmission = true;
 
 collectionChanges(usersCollection)
     .subscribe((docChanges: DocumentChange[]) => {
+        if (initialEmission) {
+            console.log(`${docChanges.length} docs in collection`);
+            initialEmission = false;
+            return;
+        }
+
+        if (docChanges.length > 1) {
+            console.log(`${docChanges.length} docs have been ${docChanges[0].type}`);
+            return;
+        }
+
         docChanges.forEach(docChange => {
             console.log(`Document ${docChange.doc.id} has been ${docChange.type}`);
         });
